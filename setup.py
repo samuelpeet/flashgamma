5c051cfb63cd0a93d964c4b211945d72de859e58
Sam Peet <samuelpeet@gmail.com>
2019-02-17 16:52:35 +1000

    Don't enforce cython in setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,37 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
-from Cython.Build import cythonize
 import numpy
+# from Cython.Build import cythonize
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+try:
+    # from Cython.Distutils.extension import Extension
+    from Cython.Distutils import build_ext
+except ImportError:
+    from setuptools import Extension
+    USING_CYTHON = False
+else:
+    USING_CYTHON = True
+
+ext = '.pyx' if USING_CYTHON else '.c'
 
 extensions = [
     Extension(
         "flashgamma.gamma_evaluation_c",
-        ["flashgamma/gamma_evaluation_c.pyx"],
+        ["flashgamma/gamma_evaluation_c" + ext],
         include_dirs=[numpy.get_include()],
     ),
 ]
 
+if USING_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="flashgamma",
-    version="0.0.2",
+    version="0.0.6",
     author="Samuel Peet",
     author_email="samuel.peet.physics@gmail.com",
     description="Analysis of radiotherapy dose distributions",
@@ -26,13 +41,12 @@ setup(
     packages=find_packages(),
     install_requires=[
         'numpy',
-        'scipy',
-        'matplotlib'
+        'scipy'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    ext_modules = cythonize(extensions, gdb_debug=True)
+    ext_modules = extensions
 )
